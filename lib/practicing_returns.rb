234d8cf0d451c060e84891714bb087302d630ca2
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 03:42:12 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,13 +1,2 @@
 require 'pry'
 
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-
-hello(list) {|i| "Hello #{i} what's up?"}
